73f57f82755f3357370338a3b5275fe45ce90e89
Eric Handley <erichandley@uvic.ca>
2025-09-21 18:00:36 -0700

    Set up Redis

--- src/server/src/app.controller.ts
+++ src/server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,5 +13,10 @@ export class AppController {
   @Get('health')
   async getHealth() {
     return this.appService.getHealthCheck();
+  }
+
+  @Get('cache/:key')
+  async getCacheTest(@Param('key') key: string) {
+    return this.appService.getCachedData(key);
   }
 }
