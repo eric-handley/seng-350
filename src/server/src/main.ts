343ff4020a9b8d5b5527ca2f6ef95ae6557ee327
Keagan Potash <keaganp@uvic.ca>
2025-10-05 09:27:17 -0700

    fix for port mapping that was causing server issues on my laptop

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -61,7 +61,7 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   const port = Number(process.env.PORT ?? 3000);
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.warn(`NestJS server running on http://localhost:${port}`);
   console.warn(`Swagger API documentation available at http://localhost:${port}/api-docs`);
 }
